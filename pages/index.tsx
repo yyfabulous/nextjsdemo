5d675c097ce8e0c8380738de50d33b56b2232553
yy <334656567@qq.com>
2022-03-05 23:41:51 +0800

    fix:add ignore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
+import Link from 'next/link';
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <Link href={'/about'}>Welcome to Next.js!</Link>
+    </div>
+  );
 }
 
 export default HomePage;
